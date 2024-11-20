3be9dfd2281ce745856f7b9bf7bbc561f051b9a5
liuqingpo <1432003391@qq.com>
2024-11-20 19:45:33 +0800

    考试微服务的接口

--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -4,28 +4,11 @@ import { UserService } from './user.service';
 import { RedisModule } from '@app/redis';
 import { PrismaModule } from '@app/prisma';
 import { EmailModule } from '@app/email';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthGuard, CommonModule } from '@app/common';
 import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [
-    RedisModule,
-    PrismaModule,
-    EmailModule,
-    JwtModule.registerAsync({
-      global: true,
-      useFactory() {
-        return {
-          secret: 'guang',
-          signOptions: {
-            expiresIn: '30m', // 默认 30 分钟
-          },
-        };
-      },
-    }),
-    CommonModule,
-  ],
+  imports: [RedisModule, PrismaModule, EmailModule, CommonModule],
   controllers: [UserController],
   providers: [
     UserService,
