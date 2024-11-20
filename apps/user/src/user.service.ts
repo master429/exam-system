c2cbaed7cd22600d1911abc930cd283b768c92c5
liuqingpo <1432003391@qq.com>
2024-11-20 19:18:26 +0800

    登录、鉴权、修改密码

--- apps/user/src/user.service.ts
+++ apps/user/src/user.service.ts
@@ -8,6 +8,8 @@ import {
   Logger,
 } from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
 
 @Injectable()
 export class UserService {
@@ -57,6 +59,60 @@ export class UserService {
     } catch (e) {
       this.logger.error(e, UserService);
       return null;
+    }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const foundUser = await this.prismaService.user.findUnique({
+      where: {
+        username: loginUserDto.username,
+      },
+    });
+
+    if (!foundUser) {
+      throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST);
+    }
+
+    if (foundUser.password !== loginUserDto.password) {
+      throw new HttpException('密码错误', HttpStatus.BAD_REQUEST);
+    }
+
+    delete foundUser.password;
+    return foundUser;
+  }
+
+  async updatePassword(passwordDto: UpdateUserPasswordDto) {
+    const captcha = await this.redisService.get(
+      `update_password_captcha_${passwordDto.email}`,
+    );
+
+    if (!captcha) {
+      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+    }
+
+    if (passwordDto.captcha !== captcha) {
+      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+    }
+
+    const foundUser = await this.prismaService.user.findUnique({
+      where: {
+        username: passwordDto.username,
+      },
+    });
+
+    foundUser.password = passwordDto.password;
+
+    try {
+      await this.prismaService.user.update({
+        where: {
+          id: foundUser.id,
+        },
+        data: foundUser,
+      });
+      return '密码修改成功';
+    } catch (e) {
+      this.logger.error(e, UserService);
+      return '密码修改失败';
     }
   }
 }
